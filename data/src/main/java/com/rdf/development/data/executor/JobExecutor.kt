7ff8b38ef41113f6820bd56f3681017e5a78213e
RossFeehan <ross.feehan@yahoo.com>
2019-05-18 11:48:36 +0100

    created Job Executor and packages

---
+++ data/src/main/java/com/rdf/development/data/executor/JobExecutor.kt
@@ -0,0 +1,46 @@
+package com.rdf.development.data.executor
+
+import java.util.concurrent.*
+import javax.inject.Inject
+
+
+class JobExecutor @Inject constructor() {
+
+    val threadPoolExecutor: ThreadPoolExecutor
+
+    init {
+        val workQueue: BlockingQueue<Runnable> = LinkedBlockingQueue()
+        this.threadPoolExecutor = ThreadPoolExecutor(
+            INITIAL_POOL_SIZE,
+            MAX_POOL_SIZE,
+            KEEP_ALIVE_TIME.toLong(),
+            KEEP_ALIVE_TIME_UNIT,
+            workQueue,
+            JobThreadFactory()
+        )
+    }
+
+    fun execute(runnable: Runnable) {
+        this.threadPoolExecutor.execute(runnable)
+    }
+
+    class JobThreadFactory : ThreadFactory {
+        private var counter = 0
+
+        override fun newThread(runnable: Runnable?): Thread {
+            return Thread(runnable, THREAD_NAME + counter++)
+        }
+
+        companion object {
+            val THREAD_NAME = "android_"
+        }
+    }
+
+    companion object {
+
+        val INITIAL_POOL_SIZE = 3
+        val MAX_POOL_SIZE = 5
+        val KEEP_ALIVE_TIME = 10
+        val KEEP_ALIVE_TIME_UNIT = TimeUnit.SECONDS
+    }
+}
