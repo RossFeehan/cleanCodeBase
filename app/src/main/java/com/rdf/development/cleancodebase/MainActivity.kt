98197cc6964edaa089564ecc264f658bc071bde1
RossFeehan <ross.feehan@yahoo.com>
2019-05-18 13:30:23 +0100

    Dependencies for view layer, updated to android X

--- app/src/main/java/com/rdf/development/cleancodebase/MainActivity.kt
+++ app/src/main/java/com/rdf/development/cleancodebase/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.rdf.development.cleancodebase
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
-
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,9 +15,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        fab.setOnClickListener {
+          Toast.makeText(this, "Hello world", Toast.LENGTH_LONG).show()
         }
     }
 
