674b7b4260aadbea15c73011f8433fa674be78bd
RossFeehan <ross.feehan@yahoo.com>
2019-05-19 21:01:35 +0100

    Created Single use case

---
+++ domain/src/main/java/com/rdf/development/domain/useCase/SingleUseCase.kt
@@ -0,0 +1,31 @@
+package com.rdf.development.domain.useCase
+
+import io.reactivex.Single
+
+
+abstract class SingleUseCase<T>: BaseUseCase() {
+
+    protected abstract fun build(): Single<T>
+
+    protected fun make(wrap: Boolean): Single<T> {
+        val single = wrapDebug(build())
+        return if (wrap) wrap(single) else single
+    }
+
+    fun get(): Single<T> {
+        return make(true)
+    }
+
+    fun chain(): Single<T> {
+        return make(false)
+    }
+
+    protected fun wrap(single: Single<T>): Single<T> {
+        return single.compose(schedulerTransformer().applySingleSchedulers())
+    }
+
+    private fun wrapDebug(single: Single<T>): Single<T> {
+        return single
+    }
+
+}
