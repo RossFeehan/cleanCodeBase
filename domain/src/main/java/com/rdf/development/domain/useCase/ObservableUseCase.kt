1337acc2c4c398408738b60ec5c0ea36c3c68db7
RossFeehan <ross.feehan@yahoo.com>
2019-05-19 20:56:55 +0100

    Created observable use case

---
+++ domain/src/main/java/com/rdf/development/domain/useCase/ObservableUseCase.kt
@@ -0,0 +1,34 @@
+package com.rdf.development.domain.useCase
+
+import io.reactivex.Observable
+
+
+abstract class ObservableUseCase<T>: BaseUseCase() {
+
+    protected abstract fun build(): Observable<T>
+
+    protected fun make(wrap: Boolean): Observable<T> {
+        val observable = wrapDebug(build())
+        return if (wrap) wrap(observable) else observable
+    }
+
+    fun get(): Observable<T> {
+        return make(true)
+    }
+
+    fun chain(): Observable<T> {
+        return make(false)
+    }
+
+    private fun wrap(observable: Observable<T>): Observable<T> {
+        return observable.compose(schedulerTransformer().applyObservableSchedulers())
+    }
+
+    private fun wrapDebug(observable: Observable<T>): Observable<T> {
+        // if (debugTransformer().isPresent && isDebugLogsEnabled()) {
+        //    return observable.compose(debugTransformer().get.applyObservableDebugger(getClass().getSimpleName()));
+        // }
+        return observable
+    }
+
+}
