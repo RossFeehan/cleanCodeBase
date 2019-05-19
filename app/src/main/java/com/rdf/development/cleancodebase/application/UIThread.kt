e61142774ad68be2831bca07f8d45ebd5577abb5
RossFeehan <ross.feehan@yahoo.com>
2019-05-19 21:06:46 +0100

    Created ui thread scheduler

---
+++ app/src/main/java/com/rdf/development/cleancodebase/application/UIThread.kt
@@ -0,0 +1,14 @@
+package com.rdf.development.cleancodebase.application
+
+import com.rdf.development.domain.executor.PostExecutionThread
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import javax.inject.Inject
+
+
+class UIThread @Inject constructor(): PostExecutionThread {
+
+    override fun getScheduler(): Scheduler {
+        return AndroidSchedulers.mainThread()
+    }
+}
